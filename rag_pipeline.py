5a6493e514eeb075d1e7852b95e95c6f644fa66f
SIDDI NEHA SREE <siddinehasree@gmail.com>
2026-06-16 09:21:35 +0530

    Refactor document loading and answer generation

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -5,22 +5,45 @@ def load_documents(folder):
     docs = []
 
     for file in os.listdir(folder):
-
         if file.endswith(".pdf"):
-
             reader = PdfReader(
-                os.path.join(folder,file)
+                os.path.join(folder, file)
             )
 
             text = ""
 
             for page in reader.pages:
-                text += page.extract_text()
+                page_text = page.extract_text()
+                if page_text:
+                    text += page_text
 
             docs.append({
-                "file":file,
-                "text":text,
-                "pages":len(reader.pages)
+                "file": file,
+                "text": text,
+                "pages": len(reader.pages)
             })
 
     return docs
+
+
+def generate_answer(question):
+    docs = load_documents("data")
+
+    context = ""
+
+    for doc in docs:
+        context += doc["text"] + "\n"
+
+    question = question.lower()
+
+    if "leave" in question:
+        for doc in docs:
+            if "leave" in doc["text"].lower():
+                return doc["text"][:1000]
+
+    if "travel" in question:
+        for doc in docs:
+            if "travel" in doc["text"].lower():
+                return doc["text"][:1000]
+
+    return context[:1000]
